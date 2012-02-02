8be9ee58e0dccd91ff1451699213e2943a1bb1e8
Evan Ricketts <ejr10@hampshire.edu>
2012-02-02 17:11:08 -0500

    hw03 done

--- hw03/prissybot.py
+++ hw03/prissybot.py
@@ -1,48 +1,60 @@
 #!/usr/bin/env python
+
 """
 prissybot.py
+Evan Ricketts
+for CS112
 
-CS112 Homework 3:   PrissyBot
-
-Prissy bot, the rude chat bot, is just mean!  It does not listen, asks obnoxious questions, and says anything it likes.
+ps - if i forgot some requirement, i apologize. simply let me know
+and i will add it posthaste.
 """
 
-# Step 1:
-# -----------------------
-# Program the following.
-# 
-#    $ python prissybot.py
-#    Enter your name:  Paul
-#   
-#    PrissyBot: Hello there, Paul
-#    Paul: hi bot
-#    PrissyBot: You mean, "hi bot, sir!"
-# 
-# Make sure the user inputs their own name and responses.
-
-
-
-# Step 2:
-# -----------------------
-# Keep adding to the conversation. Make sure that your program 
-# includes the following:
-# 
-#  * get and use input from the user
-#  * 3 math problems
-#     * at least one should get numbers from the user
-#  * at least 3 insults
-
-
-# Advanced
-# -------------------------
-# Make sure your prissy bot uses string formatting throughout.  
-# Also, create new programs for the following:
-#  
-#  1. draw some kind of ascii art based on user input
-#  2. print a decimal/binary/hexidecimal conversion table 
-#     * well formated and labeled
-#     * reads 5 numbers from the input (all less than 256)
-#  3. reduce a fraction
-#     * read a numerator and denominator from the user
-#     * ex.  6/4 = 1 2/4
-
+name=raw_input("Enter your name: ")          #assigns input to variable 'name'
+print ""
+print "Prissybot: Hello there, "+name+"."
+print "(you can respond if you'd like..)"
+say1=raw_input(name+": ")
+print "Prissybot: You mean '"+say1+", sir!'"
+#print "(dare you respond?)"
+say2=raw_input(name+": ")
+print "Prissybot: Brilliant! '"+say2+".' I'm surprised NASA let you leave..."
+say3=raw_input(name+": ")
+print "Prissybot: '"+say3+"?' Really? That's the best you can do?"
+say4=raw_input(name+": ")
+print "Prissybot: you type like a drunk. I mean who even says '"+say4+"'?"
+say5=raw_input(name+": ")
+print "Prissybot: Well if you're not going to even try I'm done with this game"
+print "I am now... AN ADDING MACHINE!"
+print "Addbot: Give me 3 whole numbers to sum!"
+v1=raw_input("Variable 1: ")
+v2=raw_input("Variable 2: ")
+v3=raw_input("Variable 3: ")
+i1=int(v1)
+i2=int(v2)
+i3=int(v3)
+sum=i1+i2+i3
+sum1=str(sum)
+print "Addbot: The sum of your variables is "+sum1+"."
+print "Addbot: Now lets do some subtraction. Enter in order three numbers"
+print "to be subtracted from the other."
+n1=raw_input("Number 1: ")
+n2=raw_input("Number 2: ")
+n3=raw_input("Number 3: ")
+a1=int(n1)
+a2=int(n2)
+a3=int(n3)
+bum=a1-a2-a3
+bum1=str(bum)
+print "Addbot: The difference of your variables is "+bum1+"."
+print "Addbot: Now for the grand finale, I will divide!"
+print "Addbot: Remember, in A/B, A is the dividend and B is the divisor."
+d=raw_input("Dividend: ")
+v=raw_input("Divisor: ")
+d1=float(d)
+v1=float(v)
+q=d1/v1
+q1=str(q)
+print "Addbot: The quotient of your equation is (roughly) "+q1+"."
+print "I grow weary. Goodnight!"
+say6=raw_input(name+": ")
+print "Addbot: I'm out of the sass game now, don't you get it kid?!"
