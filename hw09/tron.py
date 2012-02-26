9fa7486edbae8866f587baf54c1770684b7f831e
Evan Ricketts <ejr10@hampshire.edu>
2012-02-26 12:31:21 -0500

    started; got splashscreen

--- hw09/tron.py
+++ hw09/tron.py
@@ -6,4 +6,63 @@ The simple game of tron with two players.  Press the space bar to start the game
 """
 
 import pygame
+import math
+import random
+import time
+from pygame import draw
 from pygame.locals import *
+
+def draw_players():
+    return
+
+def crash():
+    return
+
+def win_message():
+    return
+
+def scorekeeper():
+    return
+
+def game():
+    screen.fill(BLACK)
+    print "now in the game loop"
+    print "finished"
+    return
+
+def textrenderer(text,x,y,color,fontsize):
+   font = pygame.font.SysFont("Arial",fontsize)
+   ren = font.render(text,1,color)
+   screen.blit(ren, (x,y))
+
+BLACK = (0,0,0)
+
+pygame.init()
+pygame.font.init()
+
+screen = pygame.display.set_mode((600,600))
+clock = pygame.time.Clock()
+done = False
+screen_bounds = screen.get_rect()
+
+screen.fill((BLACK))
+
+p1lightbike = []
+p2lightbike = []
+
+while not done:
+   
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            done = True
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            done = True
+        elif event.type == KEYDOWN and event.key == K_SPACE:
+            game()
+    
+    textrenderer("TRON",50,55,(0,0,255),170)
+    textrenderer("a clone",87,220,(238,0,15),50)
+    textrenderer("press the spacebar to begin",171,420,(249,255,250),20)
+
+            
+    pygame.display.flip()
