3e4aed48c308af1c42e0e30baf32c780d716b668
Evan Ricketts <ejr10@hampshire.edu>
2012-03-04 01:00:03 -0500

    need to add comments, 0s feature

--- hw11/minesweeper.py
+++ hw11/minesweeper.py
@@ -19,11 +19,13 @@ def print_grid(grid):
         print ""
 
 def d_flag(x,y):
-   pygame.draw.polygon(screen,(220,0,0),((x+7,y-7),(x,y-8),(x+7,y-14),
+    x += 8
+    y += 19
+    pygame.draw.polygon(screen,(220,0,0),((x+7,y-7),(x,y-8),(x+7,y-14),
                                           (x+7,y-7)))
-   pygame.draw.line(screen,(220,0,0),(x+6,y-4),(x+6,y-8),2)
-   pygame.draw.line(screen,BLACK,(x+3,y-3),(x+9,y-3),2)
-   pygame.draw.line(screen,BLACK,(x,y),(x+12,y),4)
+    pygame.draw.line(screen,(220,0,0),(x+6,y-4),(x+6,y-8),2)
+    pygame.draw.line(screen,BLACK,(x+3,y-3),(x+9,y-3),2)
+    pygame.draw.line(screen,BLACK,(x,y),(x+12,y),4)
 
 def d_bomb(x,y):
     pygame.draw.circle(screen, BLACK, (x,y), 6)
@@ -35,19 +37,50 @@ def d_bomb(x,y):
 
 def draw_grid():
     #draw squares
+        over = False
         i = 0
         x = 18
         y = 150
         pygame.draw.line(screen,WHITE,(x,y-2),(x+300,y-2),2)
         for z in range(10):
             for i in range(10):
+                fX, fY, p, pp = tile_rects[z][i]
                 pygame.draw.line(screen,WHITE,(x,y),(x,y+28),2)
-                pygame.draw.rect(screen,BORDER_GRAY,tile_rects[z][i])
+                if tile_state[z][i] == 0:
+                    pygame.draw.rect(screen,BORDER_GRAY,tile_rects[z][i])
+                elif tile_state[z][i] == 1:
+                    pygame.draw.rect(screen,(193,193,193),tile_rects[z][i])
+                    text = str(grid[z][i])
+                    TEXT_COLOR = 0,0,0
+                    if text == 'B':
+                        d_bomb(fX+13,fY+15)
+                        over = True
+                    elif text == '0':
+                        TEXT_COLOR = 0,0,0
+                        render_font(text,fX+7,fY+4,TEXT_COLOR,31)
+                    elif text == '1':
+                        TEXT_COLOR = 0,0,255
+                        render_font(text,fX+7,fY+4,TEXT_COLOR,31)
+                    elif text == '2':
+                        TEXT_COLOR = 0,128,0
+                        render_font(text,fX+7,fY+4,TEXT_COLOR,31)
+                    elif text == '3':
+                        TEXT_COLOR = 0,0,255
+                        render_font(text,fX+7,fY+4,TEXT_COLOR,31)
+                    elif text == '4':
+                        Text_COLOR = 0,0,128
+                        render_font(text,fX+7,fY+4,TEXT_COLOR,31)
+                    
+                elif tile_state[z][i] == 2:
+                    pygame.draw.rect(screen,BORDER_GRAY,tile_rects[z][i])
+                    d_flag(fX,fY)
                 x += 30
             pygame.draw.line(screen,WHITE,(x,y),(x,y+28),2)
             x = 18
             y += 30
             pygame.draw.line(screen,WHITE,(x,y-2),(x+300,y-2),2)
+        return over
+            
 
 
 
@@ -99,6 +132,8 @@ while count < 10:  #while loop inserts bombs in logic grid
         grid[x][y] = 'B'
         count += 1
 
+game_OVER = False
+
 #places numbers in logic grid
 test_cords = ((-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,0),(1,1),(1,-1))
 for y in range(10):
@@ -123,12 +158,6 @@ for z in range(10):
     x = 18
     y += 30
     tile_rects.append(row)
-    
-
-
-print_grid(grid)
-#x, y, z, p = tile_rects[1][1]
-#print x,y,z,p
 
 ###############
 ## game loop ##
@@ -222,18 +251,15 @@ while not done:
                     for y in range(10):
                         rect = tile_rects[x][y]
                         if rect.collidepoint(pygame.mouse.get_pos()):
-                            uncover = x,y
-                            flag = 0
-                print uncover,'uncover'
+                            tile_state[x][y] = 1
             elif evt.type == MOUSEBUTTONDOWN and evt.button == 3: #flag
                 for x in range(10):
                     for y in range(10):
                         rect = tile_rects[x][y]
                         if rect.collidepoint(pygame.mouse.get_pos()):
-                            flag = x,y
-                            uncover = 0
-                print flag,'flag'
-            #elif evt.type == MOUSEBUTTONDOWN:
+                            tile_state[x][y] = 2
+                
+            
                 
                
                     
@@ -242,12 +268,35 @@ while not done:
 
 
         #draw
-        draw_grid()
+        
+        game_OVER = draw_grid()
         
         EXIT_COLOR = WHITE
         if exit_rect.collidepoint(pygame.mouse.get_pos()):
             EXIT_COLOR = BORDER_GRAY
         render_font("EXIT",146,474,EXIT_COLOR,27)
+        
+        while (game_OVER):
+            x = 29
+            y = 52
+            pygame.draw.rect(screen,BLACK,((x,y),(280,60)))
+            render_font("GAME  OVER",x+7,y+9,WHITE,60)
+            for evt in pygame.event.get():
+                if evt.type == MOUSEBUTTONDOWN:
+                    if exit_rect.collidepoint(pygame.mouse.get_pos()):
+                        game_OVER = False
+                        game = False
+                elif evt.type == KEYDOWN and evt.key == K_ESCAPE:
+                    game_OVER = False
+                    game = False
+            EXIT_COLOR = WHITE
+            if exit_rect.collidepoint(pygame.mouse.get_pos()):
+                EXIT_COLOR = BORDER_GRAY
+            render_font("EXIT",146,474,EXIT_COLOR,27)       
+            
+            pygame.display.flip()
+            clock.tick(FPS)
+            
 
         pygame.display.flip()
         clock.tick(FPS)
