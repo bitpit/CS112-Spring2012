456232ad52811e454713ab78f288f4188a688487
Evan Ricketts <ejr10@hampshire.edu>
2012-03-15 13:50:36 -0400

    work

---
+++ class stuff/day13.py
@@ -0,0 +1,14 @@
+designing software i guess
+
+how would you model hampshire:
+
+empty bottles, students, faculty, bad jokes, staff, buildings, courses, 
+resedential,academic buildings, div 1-3, social justice, dropouts, alumni,
+semesters, deathfest, chairs, meds, fire, committees, President lash, 
+
+
+
+
+Superclass
+
+basically you just draw cards of attributes and communication and shit
